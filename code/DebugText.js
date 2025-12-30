db5730da5fc8a5eb75ddbe686897c8a6c815e988
John C <pieboy4444@yahoo.co.uk>
2025-12-30 01:03:22 +0000

    Fixed #43 Almost... will create seperate issue. The sprite arrays should be physics "groups" in phaser for performance.
    Added bullet manager, very bare bones, creates bullets on startup and "fires" them when called by the ship.
    
    Removed collisionmanager, after I have trimmed the event handling code to delegate it to the right classes it would be too small to justify it's own class.
    
    Altered DebugText, now it accesses what it needs via an "interface" (basically a getter function) which should help decouple the two classes.
    
    Now all the ships are in one array, same with bullets, should make collision detection code much nicer.
    
    Added "name" to ships (their sprite name) to help debug later on.
    
    AIController now uses the getter interface.

--- code/DebugText.js
+++ code/DebugText.js
@@ -43,28 +43,29 @@ class DebugText extends Phaser.GameObjects.Text {
                         "2 for battle music" : "-",
                         "3 for stealth music": "-",
                         "4 for boss music": "-",
-                        "(LEFT / RIGHT) THRUST SPEED": this.scene.player.THRUST_SPEED,
-                        "(UP / DOWN) MAX SPEED" : this.scene.player.MAX_SPEED,
-                        "(Q / E) TURN SPEED FACTOR" : this.scene.player.TURN_SPEED_FACTOR,
-                        "G to show/hide physics bodies" : "-"
+                        "(LEFT / RIGHT) THRUST SPEED": this.scene.getPlayer().THRUST_SPEED,
+                        "(UP / DOWN) MAX SPEED" : this.scene.getPlayer().MAX_SPEED,
+                        "(Q / E) TURN SPEED FACTOR" : this.scene.getPlayer().TURN_SPEED_FACTOR,
+                        "G to show/hide physics bodies" : "-",
+                        "F to enable/disable enemy AI" : "-"
                     };
         
         this.testVars[1] = {
                         "-----------DEBUG-----------": "-",
-                        "Player X" : this.scene.player.x,
-                        "Player Y" : this.scene.player.y,
-                        "Player Thruster X" : this.scene.player.tX,
-                        "Player Thruster Y" : this.scene.player.tY,
-                        "Target Angle" : this.scene.player.controller.targetAngle,
-                        "Player Angle" : this.scene.player.rotation,
-                        "Player Velocity X" : this.scene.player.body.velocity.x,
-                        "Player Velocity Y" : this.scene.player.body.velocity.y,
+                        "Player X" : this.scene.getPlayer().x,
+                        "Player Y" : this.scene.getPlayer().y,
+                        "Player Thruster X" : this.scene.getPlayer().tX,
+                        "Player Thruster Y" : this.scene.getPlayer().tY,
+                        "Target Angle" : this.scene.getPlayer().controller.targetAngle,
+                        "Player Angle" : this.scene.getPlayer().rotation,
+                        "Player Velocity X" : this.scene.getPlayer().body.velocity.x,
+                        "Player Velocity Y" : this.scene.getPlayer().body.velocity.y,
                         "Mouse Buttons" : this.scene.game.input.mousePointer.buttons,
-                        "Cursor X (Screen)" : this.scene.player.controller.cursorPos.x, 
-                        "Cursor Y (Screen)" : this.scene.player.controller.cursorPos.y,
-                        "Player Boost" : this.scene.player.isBoost,
-                        "Player Brake" : this.scene.player.isBrake,
-                        "Enemy AI Active?" : this.scene.enemies[0].isActive 
+                        "Cursor X (Screen)" : this.scene.getPlayer().controller.cursorPos.x, 
+                        "Cursor Y (Screen)" : this.scene.getPlayer().controller.cursorPos.y,
+                        "Player Boost" : this.scene.getPlayer().isBoost,
+                        "Player Brake" : this.scene.getPlayer().isBrake,
+                        "Enemy AI Active?" : this.scene.ships[1].isActive 
 
                     };
 
